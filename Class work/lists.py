3a2820cbcef0acb378fffc8a8b5f3c7701b212c7
badugudivyateja <badugudivyateja04@gmail.com>
2025-11-13 21:25:35 +0530

    lists

---
+++ Class work/lists.py
@@ -0,0 +1,196 @@
+# List creation
+l = []
+l = list()
+l = [1, 2, 3, 4, 5, 6]
+l = list([1, 2, 3, 4, 5])
+l
+[1, 2, 3, 4, 5]
+
+# Nested lists
+l = [[1, 2], [3, 4], [5, 6]]
+l = [1, 1, 1, 1, 1, 1, 2]
+l
+[1, 1, 1, 1, 1, 1, 2]
+
+# Concatenation and repetition
+[1, 2, 3] + [4, 5, 6]
+[1, 2, 3, 4, 5, 6]
+[1, 2] * 5
+[1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+
+# Membership
+2 in l
+True
+6 in l
+False
+
+# Indexing and slicing
+l = ['ajay', 'krishna', 'shekar', 'santhosh', 'harsha', 'varun', 'shiva']
+l[2]
+'shekar'
+l[-3]
+'harsha'
+l[-2]
+'varun'
+l[-1]
+'shiva'
+l[1]
+'krishna'
+l[0]
+'ajay'
+l[1:3]
+['krishna', 'shekar']
+l[-1:-4:-1]
+['shiva', 'varun', 'harsha']
+l[::2]
+['ajay', 'shekar', 'harsha', 'shiva']
+l[1::2]
+['krishna', 'santhosh', 'varun']
+l[::-1]
+['shiva', 'varun', 'harsha', 'santhosh', 'shekar', 'krishna', 'ajay']
+
+# Nested lists again
+l = [[1, 2], [3, 4], [5, 6]]
+l[2]
+[5, 6]
+l[1]
+[3, 4]
+l[0]
+[1, 2]
+l[0][0]
+1
+l[2][0]
+5
+l[1][1]
+4
+
+# Mutable and heterogeneous list
+l = [10, 20, 30, 40, 50]
+id(l)
+l[1]
+20
+l[1] = 20.3
+l
+[10, 20.3, 30, 40, 50]
+id(l)
+l[2] = 30 + 4j
+l
+[10, 20.3, (30+4j), 40, 50]
+l[3] = 'string'
+l
+[10, 20.3, (30+4j), 'string', 50]
+
+# Append examples
+l.append([1, 2, 3])
+l
+[10, 20.3, (30+4j), 'string', 50, [1, 2, 3]]
+l.append((10, 202, 30))
+l
+[10, 20.3, (30+4j), 'string', 50, [1, 2, 3], (10, 202, 30)]
+l.append(70)
+l.append(80)
+l
+[10, 20.3, (30+4j), 'string', 50, [1, 2, 3], (10, 202, 30), 70, 80]
+
+# Extend and insert
+l.extend([100, 90, 20, 10])
+l
+[10, 20.3, (30+4j), 'string', 50, [1, 2, 3], (10, 202, 30), 70, 80, 100, 90, 20, 10]
+l.insert(0, 10)
+l
+[10, 10, 20.3, (30+4j), 'string', 50, [1, 2, 3], (10, 202, 30), 70, 80, 100, 90, 20, 10]
+l.insert(5, {1: 2, 2: 4, 3: 6, 4: 8})
+l.insert(8, {1, 2, 3, 4})
+l
+[10, 10, 20.3, (30+4j), 'string', {1: 2, 2: 4, 3: 6, 4: 8}, 50, [1, 2, 3], {1, 2, 3, 4}, (10, 202, 30), 70, 80, 100, 90, 20, 10]
+
+# Remove and pop
+l.remove(10)
+l.remove((10, 202, 30))
+l.remove({1, 2, 3, 4})
+l.remove(100)  # removed once
+l.pop(2)
+l.pop(1)
+l.pop(1)
+l.pop(1)
+l.pop()
+l.pop()
+l.pop()
+l.pop()
+l
+[10, 50, 70]
+
+# Delete, clear, del
+del l[2]
+l
+[10, 50]
+l.clear()
+l
+[]
+l = [1, 2, 3, 4]
+del l
+
+# Index, count, sort, reverse
+l = [10, 20, 30, 40, 50, 60, 70, 80]
+l.index(30)
+2
+l.index(80)
+7
+l.append(10)
+l.index(10)
+0
+l.count(10)
+2
+l.sort()
+l
+[10, 10, 20, 30, 40, 50, 60, 70, 80]
+
+l = [8, 2, 3, 4, 1, 8, 3, 4]
+sorted(l)
+[1, 2, 3, 3, 4, 4, 8, 8]
+l.sort()
+l
+[1, 2, 3, 3, 4, 4, 8, 8]
+l.sort(reverse=True)
+l
+[8, 8, 4, 4, 3, 3, 2, 1]
+l.reverse()
+l
+[1, 2, 3, 3, 4, 4, 8, 8]
+
+# Copy vs assignment
+a = [1, 2, 3, 4, 5]
+b = a
+b.append(6)
+a
+[1, 2, 3, 4, 5, 6]
+c = a.copy()
+c.append(9)
+c
+[1, 2, 3, 4, 5, 6, 9]
+a
+[1, 2, 3, 4, 5, 6]
+
+# Built-in functions
+l = [1, 2, 3, 3, 4, 4, 8, 8]
+max(l)
+8
+min(l)
+1
+len(l)
+8
+
+# any() and all()
+l = [0, 0.0, '', [], (), {}, set(), False]
+any(l)
+False
+all(l)
+False
+l.append(1)
+any(l)
+True
+all(l)
+False
+a = [1, 2, 3, 4, 5, 6]
+all(a)
+True
